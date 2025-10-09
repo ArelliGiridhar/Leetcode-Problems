41481dda6cf4d3cae7b3f7dfff4b0f48c7375308
ArelliGiridhar <165773721+ArelliGiridhar@users.noreply.github.com>
2025-10-09 10:18:10 +0530

    Time: 1 ms (86.31%) | Memory: 41.4 MB (75.74%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int min=Integer.MAX_VALUE;
+       // int min=Integer.MAX_VALUE;//intialize the minimum one with the maximum value why because we are finding the minmum no of moves
         int left=0;
         //i right=k-1;nt
         int curcount=0;
@@ -9,7 +9,7 @@ class Solution {
                 curcount++;
             }
         }
-        min=curcount;
+        int min=curcount;
         for(int i=k;i<blocks.length();i++){
             if(blocks.charAt(left)=='W'){
                 curcount--;
@@ -23,3 +23,4 @@ class Solution {
         return min;
     }
 }
+//intialize the first for loop from i=0 to i=k then count the no of whites in it , then intialize the next for loop from i=k to i=string length. if the i value is white increment the count value, if the left(starting index) value is white then decrement the count value update the min value in each iterations;
