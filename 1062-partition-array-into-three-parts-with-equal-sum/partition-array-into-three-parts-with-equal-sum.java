164ed2be01725b1e1a5576c0383883937964b87c
ArelliGiridhar <165773721+ArelliGiridhar@users.noreply.github.com>
2025-12-06 10:01:27 +0530

    Time: 2 ms (42.28%) | Memory: 55.6 MB (9.53%) - LeetSync

--- 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.java
+++ 1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.java
@@ -1,3 +1,11 @@
+/* Steps for this problem 
+1.First find the sum of the elements in the array 
+2.Then then check if sum is divisible by 3 if no then return false else go far partion 
+3.Then divide sum/3(why dividing 3? because in question it is given to make three parts).
+4.take a for loop from starting index to end and sum the elemnets if the co equal to 
+sum/3 then incrment the count and make co =0; and report the procedure upto n values.
+5.At final if the count is greater than or equal to 3 then return true or return false.
+*/
 class Solution {
     public boolean canThreePartsEqualSum(int[] arr) {
         int count=0;
